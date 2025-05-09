ad1839848ec82ea7f131e50ba57e99a406bfde1a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-05-09 06:31:34 +0200

    Update fr_CA, thanks to bgoutier

---
+++ language/fr_CA/plugin.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%d Add tags: %s'] = '%d Mots-clés ajoutés : %s';
+$lang['%d Not add tags: %s'] = '%d Mots-clés non ajoutés : %s';
+$lang['Add tags Mass'] = 'Ajouter des Mot-clés en masse';
+$lang['No tags'] = 'Pas de Mots-clés dans le formulaire';
